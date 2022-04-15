b75ac28bf63179256aecfc639ef44a9400e3716e
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-04-15 12:09:50 -0700

    Add solution for CheckingUnique

---
+++ CheckUnique.js
@@ -0,0 +1,26 @@
+// Write an algorithm that determines whether all the elements in a string are unique. You may not convert the string into an array or use array methods to solve this problem. The algorithm should return a boolean.
+
+// Example
+// Input: "hello"
+
+// Output: false
+
+// Input: "copyright"
+
+// Output: true
+
+function checkUnique(input)
+{
+  let output="true";
+  for(let eachChar of input)
+  {
+    //use includes(searchstring,postionFromWhichToSeach)
+    if(input.includes(eachChar,(input.indexOf(eachChar))+1))
+    {
+      output = "false";  
+    }
+  }
+  return output;
+}
+checkUnique("hello");
+checkUnique("copyright");
